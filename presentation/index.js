4f59e0610b413bd1d860e1c7d3df891e47b2715d
Zélia Geerts <zelia.geerts@citymeo.fr>
2018-06-18 16:52:09 +0200

    fix(slide): add part of presentation

--- presentation/index.js
+++ presentation/index.js
@@ -2,17 +2,7 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from "spectacle";
+import { BlockQuote, Cite, Deck, Heading, Slide, Text } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
@@ -20,51 +10,54 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "black",
-  secondary: "white",
-  tertiary: "#d347a7",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "black",
+    secondary: "white",
+    tertiary: "#d347a7",
+    quarternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+        bgColor="primary"
+      >
+        <Slide transition={["zoom"]}>
+          <Heading size={1} fit caps textColor="secondary">
             Introduction to Apollo Client
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             "best way to use GraphQL to build client application"
           </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <br />
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Cite textColor="secondary">Zélia Geerts @liliatux</Cite>
           </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading size={6} textColor="tertiary" caps>
+            "What is this ?"
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading size={6} textColor="tertiary" caps>
+            "How it's work ?"
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading size={6} textColor="tertiary" caps>
+            "And with React ?"
+          </Heading>
         </Slide>
       </Deck>
     );
